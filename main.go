1496affb466b0e13d1c015e8f96d29c9ce90cb9f
Vikas Choudhary <vikaschd@amazon.com>
2022-09-06 12:12:01 +0530

    "ping added"

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"net/http"
-	"time"
-	"io"
 	"fmt"
-	"strconv"
+	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
-}
-
-// Route declaration
-func router() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
-	return r
-}
-
-func startRouter(port int){
-	router := router()
-        srv := &http.Server{
-                Handler: router,
-                Addr:    "127.0.0.1:"+strconv.Itoa(port),
-                WriteTimeout: 15 * time.Second,
-                ReadTimeout:  15 * time.Second,
-        }
-	fmt.Println("Starting Server at:",port)
-        srv.ListenAndServe()
-}
-
-// Initiate web server
 func main() {
-	startRouter(8080)
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Pong")
+	})
+	fmt.Println("Starting server...")
+	http.ListenAndServe(":8080", nil)
 }
